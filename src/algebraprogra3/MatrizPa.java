2edecc13876b585f79b96dedb3bdf080cb49a692
josemurillo <josemurillo@192.168.1.58>
2016-06-09 20:23:33 -0600

    calcula matrizP

---
+++ src/algebraprogra3/MatrizPa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algebraprogra3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author josemurillo
+ */
+public class MatrizPa {
+    Matriz matrizL;
+    Matriz matrizU;
+    Matriz matrizP;
+    Matriz p;
+    double[][] respuestas;
+    
+    void calcularP(Matriz pmatrizA){
+        FactorizacionLU flu = new FactorizacionLU();
+        try{
+        flu.factorizar(pmatrizA);
+        matrizP = flu.getMatrizP();
+        
+    }catch (Exception e){
+       JOptionPane.showMessageDialog(null, "La matriz P no se pudo calcular");
+    }
+
+    }
+    
+    public Matriz getP(){
+        return matrizP;
+    }
+    
+    
+}
